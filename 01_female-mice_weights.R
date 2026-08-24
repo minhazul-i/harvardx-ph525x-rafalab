337b6287658d2a19f17c10a687d00a0911da1e78
minhazul-i <minhazul.i.med@gmail.com>
2026-08-24 14:47:09 +0600

    Update 01_female-mice_weights.R

--- 01_female-mice_weights.R
+++ 01_female-mice_weights.R
@@ -1,15 +1,15 @@
 library(tidyverse)
 dat <- read.csv("data/femaleMiceWeights.csv")
 
-// Filter the mices under the chow diet
+# Filter the mices under the chow diet
 controls <- filter(dat, Diet == "chow")
 
-// Obtain only the body weights of the controls
+# Obtain only the body weights of the controls
 select(controls, Bodyweight)
 
-// convert the bodyweights into vector
+# convert the bodyweights into vector
 controls <- select(controls, Bodyweight)
 unlist(controls)
 
-//do the whole thing using pipe in just a line
+# do the whole thing using pipe in just a line
 controls <- filter(dat, Diet == "chow") %>% select(Bodyweight) %>% unlist
