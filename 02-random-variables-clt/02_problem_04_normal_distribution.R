53bad1c1ceb0ca560f1b5bd1ef1325446240b712
Minhazul Islam <minhazul.i.med@gmail.com>
2026-08-27 09:53:48 +0600

    Create 02_problem_04_normal_distribution.R

---
+++ 02-random-variables-clt/02_problem_04_normal_distribution.R
@@ -0,0 +1,13 @@
+# Week 2: Random Variables and Central Limit Theorem
+# Problem_04 Normal distribution
+
+# Suppose the height of a population are approximately equally distributed. Mean height is 165 cm and SD is 7 cm.   
+# You randomly select one person. What is the probability that their height is greater than 175 cm?
+
+library(tidyverse)
+
+# The probability that their height is less than or eqaul to 175
+pnorm(175, 165, 7)
+
+# so, the probability that their height is greater than 175 cm
+1 - pnorm(175, 165, 7)
