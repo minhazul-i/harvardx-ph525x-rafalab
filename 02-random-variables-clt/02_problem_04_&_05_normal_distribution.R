047a9d20e03551fed0ca8ca39dc95c66366df457
Minhazul Islam <minhazul.i.med@gmail.com>
2026-08-27 10:05:33 +0600

    Update 02_problem_04_&_05_normal_distribution.R

--- 02-random-variables-clt/02_problem_04_&_05_normal_distribution.R
+++ 02-random-variables-clt/02_problem_04_&_05_normal_distribution.R
@@ -11,3 +11,13 @@ pnorm(175, 165, 7)
 
 # so, the probability that their height is greater than 175 cm
 1 - pnorm(175, 165, 7)
+
+
+
+
+# Problem_05 Normal distribution
+
+# Adult weights are approximately normally distributed with mean = 60 kg, and SD = 8 kg
+# what is the probability that a randomly selected adult weighs between 60 and 75 kg?
+
+pnorm(75, 60, 8) - pnorm(60, 60, 8)
